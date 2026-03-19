1f6ef168c9d6bddbfece83024e7cba650ac6bc0e
Kai Lin Jessica Ong (qris-uq) <s4575250@bun121.hpc.net.uq.edu.au>
2026-03-20 01:28:30 +1000

    update sccoda and milo analysis, subset b cells

---
+++ scripts/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+echo "Running CD4 Milo..."
+python -m nbconvert --to notebook --execute --inplace "/scratch/user/s4575250/BIOX7014_thesis/notebooks/03_abundance_change/PICA Batch001-Batch007/CD4/03_PICA_CD4_milo_pertpy.ipynb" --ExecutePreprocessor.timeout=-1 --log-level=INFO
+echo "CD4 milo done"
+
+echo "Running CD4 Milo age..."
+python -m nbconvert --to notebook --execute --inplace "/scratch/user/s4575250/BIOX7014_thesis/notebooks/03_abundance_change/PICA Batch001-Batch007/CD4/03_PICA_CD4_milo_pertpy_age.ipynb" --ExecutePreprocessor.timeout=-1 --log-level=INFO
+echo "CD4 milo age done"
+
+echo "Running CD4 Milo sex..."
+python -m nbconvert --to notebook --execute --inplace "/scratch/user/s4575250/BIOX7014_thesis/notebooks/03_abundance_change/PICA Batch001-Batch007/CD4/03_PICA_CD4_milo_pertpy_sex.ipynb" --ExecutePreprocessor.timeout=-1 --log-level=INFO
+echo "CD4 milo sex done"
+
+echo "Running CD4 Milo interaction..."
+python -m nbconvert --to notebook --execute --inplace "/scratch/user/s4575250/BIOX7014_thesis/notebooks/03_abundance_change/PICA Batch001-Batch007/CD4/03_PICA_CD4_milo_pertpy_age_sex.ipynb" --ExecutePreprocessor.timeout=-1 --log-level=INFO
+echo "CD4 milo interaction done"
+
+echo "CD4 All done!"
